7d152f2e91fa22a3b25dcfc3e599ab60444e2e33
Kesha <mishka9514@mail.ru>
2024-12-04 09:52:45 +0800

    Initial Commit

---
+++ src/Lesson2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val a = 5
+    val b = 7
+   // println(a + b)
+    val sum= a + b
+    println(sum::class.simpleName)
+    println(10%3)
+    println(a>b)
+    println(a<b)
+    println(a==b)
+    println(a<=b)
+    println(a>=b)
+    println(a!=b)
+}
