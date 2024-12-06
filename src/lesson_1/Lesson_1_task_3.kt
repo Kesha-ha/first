3856cecdb9fe5d1c8ddc34913bc1cd5690b29fd2
Kesha <mishka9514@mail.ru>
2024-12-06 16:59:47 +0800

    first-1-3 create

--- src/lesson_1/Lesson_1_task_3.kt
+++ src/lesson_1/Lesson_1_task_3.kt
@@ -3,13 +3,13 @@ package lesson_1
 
 fun main() {
 
-    val numberOfOrders: Int = 75
-    val gratitudeMessage: String = "Спасибо за заказ! Ждем вас снова!"
-    println(numberOfOrders)
-    println(gratitudeMessage)
-    var numberOfEmployees: Int = 2000
-//    println(numberOfEmployees)
-    numberOfEmployees = 1999
-    println(numberOfEmployees)
+    val year:Int = 1961
+    var hour:Int = 9
+    var minute:Int = 7
+    println("Год полета - " + year)
+    println("Взлет - "+"%02d".format(hour)+":"+"%02d".format(minute))
+    hour = 10
+    minute = 55
+    println("Посадка - " + hour+":" +minute)
 
 }
