bd14d1ef64009ce0b7f1db3b72e0d3702a3d5735
Kesha <mishka9514@mail.ru>
2024-12-06 16:19:52 +0800

    first-1-1 create

---
+++ src/lesson_1/Lesson1_task1.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val gratitudeMessage: String = "Спасибо за заказ! Ждем вас снова!"
+    println(numberOfOrders)
+    println(gratitudeMessage)
+
+}
