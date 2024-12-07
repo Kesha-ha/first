fdcad2bd67e48a773a1ebfde7d1566d5a52431b2
Kesha <mishka9514@mail.ru>
2024-12-07 17:47:41 +0800

    first-1-5 completing task 5

--- src/lesson1/lesson1_task5.kt
+++ src/lesson1/lesson1_task5.kt
@@ -2,20 +2,24 @@ package lesson1
 
 
 fun main() {
-    val lenght:Long = 40_868_600_000
-    val age:Short = 27
-    val partOfDay:Double = 0.075
-    val secods:Int = 6480
-    val partOfYear:Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val secods: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
-    println("Расстояние - "+lenght)
-    println("Возраст - " + age)
-    println("Часть дня - " + partOfDay)
-    println("Секунды - "+secods)
-    println("Часть года - " + partOfYear)
-    println("Апогей орбиты - "+apogee)
+    val hour = secods / 3600
+    val minutes = (secods / 60) - ((secods / 3600) * 60)
+    val sec = secods - ((secods / 60) * 60)
 
-    println("Время, проведенное в космосе - "+"%02d".format(secods/3600)+":"+"%02d".format(((secods/60)-((secods/3600)*60)))+":"+"%02d".format((secods-((secods/60)*60))))
+    println("Расстояние - $lenght")
+    println("Возраст - $age")
+    println("Часть дня - $partOfDay")
+    println("Секунды - $secods")
+    println("Часть года - $partOfYear")
+    println("Апогей орбиты - $apogee")
+
+    println("Время, проведенное в космосе - $hour:$minutes:$sec")
 
 }
