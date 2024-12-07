03cf5b00641c7cc0860b812a81b8a3edb1c4dda9
Kesha <mishka9514@mail.ru>
2024-12-07 17:38:06 +0800

    first-1-4 with changes

--- src/lesson1/lesson1_task4.kt
+++ src/lesson1/lesson1_task4.kt
@@ -2,18 +2,19 @@ package lesson1
 
 
 fun main() {
-    val lenght:Long = 40_868_600_000
-    val age:Short = 27
-    val partOfDay:Double = 0.075
-    val secods:Int = 6480
-    val partOfYear:Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
 
-    println("Расстояние - "+lenght)
-    println("Возраст - " + age)
-    println("Часть дня - " + partOfDay)
-    println("Секунды - "+secods)
-    println("Часть года - " + partOfDay)
-    println("Апогей орбиты - "+apogee)
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val secods: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("Расстояние - $lenght")
+    println("Возраст - $age")
+    println("Часть дня - $partOfDay")
+    println("Секунды - $secods")
+    println("Часть года - $partOfYear")
+    println("Апогей орбиты - $apogee")
 
 }
