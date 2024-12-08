1fc75598042ba7a227cf9ad937949f10badd2dc1
Kesha <mishka9514@mail.ru>
2024-12-08 11:27:26 +0800

    first-1-5 with changes

--- src/lesson1/lesson1_task5.kt
+++ src/lesson1/lesson1_task5.kt
@@ -2,24 +2,15 @@ package lesson1
 
 
 fun main() {
-    val lenght: Long = 40_868_600_000
-    val age: Byte = 27
-    val partOfDay: Float = 0.075f
+
     val secods: Short = 6480
-    val partOfYear: Double = 2.0547945205479453E-4
-    val apogee: Int = 327000
+    val numberOfSecondsPerHour: Short = 3600
+    val numberOfMinute:Short = 60
+    val hour = secods / numberOfSecondsPerHour
+    val minutes = (secods / numberOfMinute) - ((secods / numberOfSecondsPerHour) * numberOfMinute)
+    val sec = secods - ((secods / numberOfMinute) * numberOfMinute)
+    val chengeHour = String.format("%02d", hour)
 
-    val hour = secods / 3600
-    val minutes = (secods / 60) - ((secods / 3600) * 60)
-    val sec = secods - ((secods / 60) * 60)
-
-    println("Расстояние - $lenght")
-    println("Возраст - $age")
-    println("Часть дня - $partOfDay")
-    println("Секунды - $secods")
-    println("Часть года - $partOfYear")
-    println("Апогей орбиты - $apogee")
-
-    println("Время, проведенное в космосе - $hour:$minutes:$sec")
+    println("Время, проведенное в космосе - $chengeHour:$minutes:$sec")
 
 }
