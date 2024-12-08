9fae1b7512c3123236265270769c285038ac187b
Kesha <mishka9514@mail.ru>
2024-12-08 11:10:04 +0800

    first-1-3 with changes

--- src/lesson_1/Lesson_1_task_3.kt
+++ src/lesson_1/Lesson_1_task_3.kt
@@ -5,13 +5,13 @@ fun main() {
 
     val year: Int = 1961
     var hour: String = "09"
-    var minute: Int = 7
-    var formattedMinute = minute.toString().padStart(2, '0')
+    var minute: String = "07"
+
 
     println("Год полета - $year")
-    println("Взлет - $hour : $formattedMinute")
+    println("Взлет - $hour : $minute")
     hour = "10"
-    minute = 55
+    minute = "55"
     println("Посадка - $hour : $minute")
 
 }
